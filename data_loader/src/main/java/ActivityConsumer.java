1e461c8ddef40602c89782c8ffc4dab51f098fde
Macbook Name <daoanhvu765@gmail.com>
2024-05-31 10:33:22 +0700

    first commit

---
+++ data_loader/src/main/java/ActivityConsumer.java
@@ -0,0 +1,53 @@
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import model.Activity;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ActivityConsumer {
+    private static final String TOPIC_NAME = "vdt2024";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9094";
+    private static final String GROUP_ID = "activity_consumer_group_1";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.put("specific.avro.reader", "true");
+
+        KafkaConsumer<String, Activity> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+        System.out.println("Consumer started, subscribing to topic: " + TOPIC_NAME);
+
+        try {
+            while (true) {
+                ConsumerRecords<String, Activity> records = consumer.poll(Duration.ofMillis(1000));
+
+                if (records.isEmpty()) {
+                    System.out.println("No records found in this poll.");
+                } else {
+                    System.out.println("Fetched " + records.count() + " records in this poll.");
+                    for (ConsumerRecord<String, Activity> record : records) {
+                        Activity activity = record.value();
+                        System.out.println("CONSUMED: " + activity);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            consumer.close();
+            System.out.println("Consumer closed.");
+        }
+    }
+}
---
+++ data_loader/src/main/java/ActivityProducer.java
@@ -0,0 +1,63 @@
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import model.Activity;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class ActivityProducer {
+    private static final String TOPIC_NAME = "vdt2024";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9094";
+
+    public static void main(String[] args) {
+        try {
+//            Defining props and set up Kafka Producer
+            Properties props = new Properties();
+            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+            props.put("schema.registry.url", "http://localhost:8081");
+            KafkaProducer<String, Activity> producer = new KafkaProducer<>(props);
+
+//            Get the csv file and parse it
+            InputStream inputStream = ActivityProducer.class.getResourceAsStream("/log_action.csv");
+            if (inputStream != null) {
+                try (InputStreamReader reader = new InputStreamReader(inputStream)) {
+                    CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+                    for (CSVRecord csvRecord : csvParser) {
+                        int studentCode = Integer.parseInt(csvRecord.get(0));
+                        String activity = csvRecord.get(1);
+                        int numberOfFiles = Integer.parseInt(csvRecord.get(2));
+                        String timeStamp = csvRecord.get(3);
+                        Activity currentActivity = Activity.newBuilder().setStudentCode(studentCode).setActivity(activity).setNumberOfFiles(numberOfFiles).setTimestamp(timeStamp).build();
+//                        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, csvRecord.get(0) + "," + csvRecord.get(1));
+                        ProducerRecord<String, Activity> record = new ProducerRecord<>(TOPIC_NAME, currentActivity);
+                        producer.send(record, new Callback() {
+                            @Override
+                            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                                if (exception == null) {
+                                    System.out.println("Send record: " + record.value());
+                                } else {
+                                    System.out.println("Error sending record: " + record.value());
+                                    System.out.println(exception.getMessage());
+                                }
+                            }
+                        });
+
+                        Thread.sleep(500);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
